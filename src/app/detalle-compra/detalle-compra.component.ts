f8bb7edce1326eb842c74b3554dac5c2f841658a
CrisBolaVi <atinabola0916@gmail.com>
2025-03-21 10:20:52 +0100

    detalle compra y venta

---
+++ src/app/detalle-compra/detalle-compra.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-detalle-compra',
+  imports: [],
+  templateUrl: './detalle-compra.component.html',
+  styleUrl: './detalle-compra.component.scss'
+})
+export class DetalleCompraComponent {
+
+}
---
+++ src/app/detalle-venta/detalle-venta.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-detalle-venta',
+  imports: [],
+  templateUrl: './detalle-venta.component.html',
+  styleUrl: './detalle-venta.component.scss'
+})
+export class DetalleVentaComponent {
+
+}
